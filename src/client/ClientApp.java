0ea8875598f99cb965aa3e312626437d9c0a10b9
kavin-du <ukdkcm@gmail.com>
2020-06-25 11:25:20 +0530

    create clientApp, make setPrice synchronize

--- src/client/ClientApp.java
+++ src/client/ClientApp.java
@@ -14,33 +14,34 @@ public class ClientApp {
 		
 		Socket s = new Socket("localhost", 2000);
 		
-		DataInputStream din = new DataInputStream(s.getInputStream());
+		DataInputStream din = new DataInputStream(s.getInputStream()); // read from server
 		DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // read from console
 		
-		System.out.println(din.readUTF());// printing count
-		System.out.println(din.readUTF());  // server incoming
+		System.out.print(din.readUTF());// printing "client started"
+		System.out.print(din.readUTF());  // printing "enter your name: "
 		
-		String name = br.readLine();
-		
-		dout.writeUTF(name);
+		String name = br.readLine(); // reading client name from console
+//		System.out.println(name+" sfsdfs");
+		dout.writeUTF(name); // send name to the server
 		dout.flush();
 		
-		System.out.println(din.readUTF()); // server incoming
+		System.out.print(din.readUTF()); // printing "enter symbol"
 		
-		String symbol = br.readLine();
+		String symbol = br.readLine(); // reading symbol from console
 		
-		dout.writeUTF(symbol);
+		dout.writeUTF(symbol); // send symbol to server
 		dout.flush();
 		
 		int response = Integer.valueOf(din.readUTF());
 		System.out.println("Response is "+response);
 		
 		if(response >= 0) {
-			System.out.println(din.readUTF()); // server incoming
+			System.out.print(din.readUTF()); // server incoming, "enter your bid: "
 			
-			dout.writeUTF(br.readLine());
+			
+			dout.writeUTF(br.readLine()); // read bid from console and send to server
 			dout.flush();
 		}
 		
--- src/server/ServerThreads.java
+++ src/server/ServerThreads.java
@@ -1,6 +1,7 @@
 package server;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -22,8 +23,13 @@ public class ServerThreads extends Thread {
 	@Override
 	public void run() {
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(this.s.getInputStream())); // reading from client
+//			BufferedReader br = new BufferedReader(new InputStreamReader(this.s.getInputStream())); // reading from client
 			DataOutputStream dout = new DataOutputStream(this.s.getOutputStream()); // write back to client
+			
+			
+			DataInputStream din = new DataInputStream(this.s.getInputStream()); // read from server
+			
+			
 			
 	//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // reading from console, not yet
 			System.out.println(""+counter+" client started"); // print in server
@@ -33,14 +39,18 @@ public class ServerThreads extends Thread {
 			
 			dout.writeUTF("Enter your Name:\n");
 			dout.flush();
-
-			String clientName = br.readLine();
 			
+			String clientName = din.readUTF();
+//			System.out.println("Your name is " + name2);
+			
+//			String clientName = br.readLine();
+//			
 //			System.out.println("Your name is " + clientName);
 			
 			dout.writeUTF("Enter symbol: \n");
 			dout.flush();
-			String symbol = br.readLine();
+//			String symbol = br.readLine();
+			String symbol = din.readUTF();
 			
 			int currentCost = -1;  // if not found, return -1
 			
@@ -50,14 +60,15 @@ public class ServerThreads extends Thread {
 				}
 			}
 			
-			dout.writeUTF(Integer.toString(currentCost)+"\n");
+			dout.writeUTF(Integer.toString(currentCost));
 			dout.flush();
 			
 			if(currentCost >= 0) {
 				dout.writeUTF("Enter your bid:\n");
 				dout.flush();
 				
-				int bidPrice = Integer.valueOf(br.readLine());   // readutf here
+//				int bidPrice = Integer.valueOf(br.readLine());   // readutf here
+				int bidPrice = Integer.valueOf(din.readUTF());   
 				for(Item item : ServerApp.items) {
 					if(item.getSymbol().equals(symbol)) {
 						item.setPrice(new Bid(clientName, bidPrice));
